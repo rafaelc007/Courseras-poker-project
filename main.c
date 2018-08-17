2728c37dffc275429caab9b6ca6fe6c30cd1c452
Rafael <raf07ray@gmail.com>
2018-08-17 09:56:03 -0400

    added dynamic memory

--- main.c
+++ main.c
@@ -1,28 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include "deck.h"
 
-deck_t init_deck(){
+deck_t * init_deck(int start) {
+  assert(start < 50);
   //initialize a default deck for test purposes
-  card_t cardi[5];
-  for(int i=0;i<5;i++){
-    cardi[i] = card_from_num(i+12);
-  }
+  deck_t * deck = malloc(sizeof(*deck));
+  deck->cards = malloc(5*sizeof(*deck->cards));
+  deck->n_cards = 5;
   
-  card_t * cards[] = {cardi, cardi+1, cardi+2, cardi+3, cardi+4};
+  for(int i=0;i<5;i++){
+    deck->cards[i] = malloc(sizeof(*deck->cards[i]));
+    card_t card = card_from_num(i+start);
+    *(deck->cards[i]) = card;
+  }
 
-  deck_t deck = {.cards = cards, .n_cards = 5};
   return deck;
 }
 
-int main(){
-  deck_t hand = init_deck();
+void release_deck(deck_t * deck) {
+   for(int i=0;i<5;i++){
+     free(deck->cards[i]);
+   }
+  free(deck->cards);
+  free(deck);
+}
+
+int main(int argc, char ** argv){
+  int start;
+  if(argc == 1){
+    start = 11;
+  }
+  else if (argc == 2){
+    start = atoi(argv[1]);
+  }
+  else {
+    printf("Usage: test-deck card_shift");
+    return EXIT_FAILURE;
+  }
+  deck_t * hand = init_deck(start);
   
-  float res1 = (float)rand()/RAND_MAX;
-  float res2 = (float)rand()/RAND_MAX;
-  printf("result 1: %.4f\n",res1);
-  printf("result 2: %.4f", res2);
+  print_hand(hand);
   printf("\n");
+
+  release_deck(hand);
   
   return EXIT_SUCCESS;
 }
